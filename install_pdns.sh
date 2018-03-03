1d00124546fc016178b511bf807b7d122f0887d2
Alex Golden <felix@agolden.com>
2018-03-02 20:01:02 -0500

    Test for previously set mysql password

--- install_pdns.sh
+++ install_pdns.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 set -e
 
-# generate mysql root password
-MYSQL_ROOT_PASS=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
+MYSQL_CONFIG=/etc/mysql_defaults.cnf
 
-# set defaults
-/bin/bash -c "debconf-set-selections <<< 'mysql-server mysql-server/root_password password $MYSQL_ROOT_PASS'"
-/bin/bash -c "debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASS'"
+if [ ! -f $MYSQL_CONFIG ]; then
 
-echo "MySQL root password: $MYSQL_ROOT_PASS"
+	# generate mysql root password
+	MYSQL_ROOT_PASS=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
+
+	# set defaults
+	/bin/bash -c "debconf-set-selections <<< 'mysql-server mysql-server/root_password password $MYSQL_ROOT_PASS'"
+	/bin/bash -c "debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASS'"
+
+	sed -e "s/\${MYSQL_ROOT_PASS}/$MYSQL_ROOT_PASS/" mysql_defaults.cnf > $MYSQL_CONFIG
+	chmod 400 $MYSQL_CONFIG
+
+	echo "MySQL root password: $MYSQL_ROOT_PASS"
+fi
 
 # install mysql
 apt-get update && apt-get install -y \
@@ -17,10 +25,6 @@ apt-get update && apt-get install -y \
 # fix permissions
 #mkdir -p /var/run/mysqld
 #chown mysql:mysql /var/run/mysqld
-
-MYSQL_CONFIG=/etc/mysql_defaults.cnf
-sed -e "s/\${MYSQL_ROOT_PASS}/$MYSQL_ROOT_PASS/" mysql_defaults.cnf > $MYSQL_CONFIG
-chmod 400 $MYSQL_CONFIG
 
 #mysql --defaults-extra-file=$MYSQL_CONFIG -e "CREATE DATABASE pdns"
 
